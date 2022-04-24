9f55718aa73181ce423cdc71939609d5e728c8ab
Parvej Ali <Rihanali01010@gmail.com>
2022-04-24 10:30:19 +0530

    Improve ui

--- app/src/main/java/com/rihanhack/tictactoe/MainActivity.kt
+++ app/src/main/java/com/rihanhack/tictactoe/MainActivity.kt
@@ -2,10 +2,18 @@ package com.rihanhack.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rihanhack.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        resources.getString(R.string.win_text, 0).also { binding.player1Score.text = it
+            binding.player2Score.text = it }
+        binding.drawScore.text = resources.getString(R.string.draw_text, 0)
     }
 }
