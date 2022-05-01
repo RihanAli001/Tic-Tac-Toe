567e7177fb2e5767f0ada6c61fc4a504a91d322b
Parvej Ali <Rihanali01010@gmail.com>
2022-05-01 22:07:07 +0530

    Add reset, onClick functionality and complete Game class

--- app/src/main/java/com/rihanhack/tictactoe/Game.kt
+++ app/src/main/java/com/rihanhack/tictactoe/Game.kt
@@ -2,9 +2,14 @@ package com.rihanhack.tictactoe
 
 class Game(private val player1: String, private val player2: String) {
     private val a = Array(9) { i -> i }
+    private var score1 = 0
+    private var score2 = 0
+    private var draw = 0
+    private var turn = 1
+    private var currentPlayer = player1
 
     fun makeTurn(player: String, p: Int): Boolean {
-        return if (a[p] == 0) {
+        return if (a[p - 1] == 0) {
             if (player == player1) {
                 a[p - 1] = 1
             } else {
@@ -17,7 +22,81 @@ class Game(private val player1: String, private val player2: String) {
         }
     }
 
-    fun winingStatus(player: String): Boolean {
+    fun getPlayer1(): String {
+        return player1
+    }
+
+    fun getPlayer2(): String {
+        return player2
+    }
+
+    fun getCurrentPlayer(): String {
+        return currentPlayer
+    }
+
+    fun setTurn(t: Int) {
+        turn = if (t == 1) {
+            currentPlayer = player1
+            1
+        } else {
+            currentPlayer = player2
+            2
+        }
+    }
+
+    fun nextTurn() {
+        turn = if (turn == 1) {
+            currentPlayer = player2
+            2
+        } else {
+            currentPlayer = player1
+            1
+        }
+    }
+
+    fun getTurn(): Int {
+        return turn
+    }
+
+    fun getPlayer1Score(): Int {
+        return score1
+    }
+
+    fun getPlayer2Score(): Int {
+        return score2
+    }
+
+    fun getDrawScore(): Int {
+        return draw
+    }
+
+    fun increaseDrawScore(v: Int) {
+        draw += v
+    }
+
+    fun isDraw(): Boolean {
+        for (i in a.indices){
+            if (a[i] == 0){
+                return false
+            }
+        }
+        return true
+    }
+
+    fun isWin(player: String): Boolean {
+        return if (winingStatus(player)) {
+            if (player == player1) {
+                score1++
+            } else {
+                score2++
+            }
+            true
+        } else {
+            false
+        }
+    }
+
+    private fun winingStatus(player: String): Boolean {
         var p = 1
         if (player == player2) {
             p = 2
@@ -45,7 +124,7 @@ class Game(private val player1: String, private val player2: String) {
     }
 
     fun reset() {
-        for (i in 0..8) {
+        for (i in a.indices) {
             a[i] = 0
         }
     }
--- app/src/main/java/com/rihanhack/tictactoe/MainActivity.kt
+++ app/src/main/java/com/rihanhack/tictactoe/MainActivity.kt
@@ -2,19 +2,24 @@ package com.rihanhack.tictactoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.rihanhack.tictactoe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
-    private lateinit var game : Game
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var game: Game
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        resources.getString(R.string.win_text, 0).also { binding.player1Score.text = it
-            binding.player2Score.text = it }
+        resources.getString(R.string.win_text, 0).also {
+            binding.player1Score.text = it
+            binding.player2Score.text = it
+        }
         binding.drawScore.text = resources.getString(R.string.draw_text, 0)
 
         binding.one.setOnClickListener { onClick(1) }
@@ -27,23 +32,20 @@ class MainActivity : AppCompatActivity() {
         binding.eight.setOnClickListener { onClick(8) }
         binding.nine.setOnClickListener { onClick(9) }
 
-        val player1 = "Parvej Ali"
-        val player2 = "Rihan Ali"
-        game = Game(player1, player2)
+        game = Game("Parvej Ali", "Rihan Ali")
+        shufflePlayer()
+        binding.resetGame.setOnClickListener { reset() }
+        reset()
+    }
 
-        if ((0..1).random() == 0) {
-            binding.currentPlayer.text = player1
-        }else{
-            binding.currentPlayer.text = player2
-        }
-
-        binding.resetGame.setOnClickListener {
-            game.reset()
-            reset()
-        }
+    private fun shufflePlayer() {
+        if ((1..2).random() == 1) game.setTurn(1) else game.setTurn(2)
+        binding.currentPlayer.text =
+            resources.getString(R.string.player_turn, game.getCurrentPlayer())
     }
 
     private fun reset() {
+        game.reset()
         binding.one.setImageDrawable(null)
         binding.two.setImageDrawable(null)
         binding.three.setImageDrawable(null)
@@ -55,13 +57,67 @@ class MainActivity : AppCompatActivity() {
         binding.nine.setImageDrawable(null)
     }
 
-    private fun onClick(v : Int){
-        val player = binding.currentPlayer.text.toString()
-        val turn = game.makeTurn(player, v)
+    private fun onClick(v: Int) {
+        println(game.getCurrentPlayer())
+        val change = game.makeTurn(game.getCurrentPlayer(), v)
 
-        if (!turn){
+        println("Change: $change")
+        if (!change) {
             return
         }
-        
+
+        val img = if (game.getTurn() == 1) {
+            R.drawable.circle
+        } else {
+            R.drawable.cross
+        }
+
+        when (v) {
+            1 -> binding.one.setImageResource(img)
+            2 -> binding.two.setImageResource(img)
+            3 -> binding.three.setImageResource(img)
+            4 -> binding.four.setImageResource(img)
+            5 -> binding.five.setImageResource(img)
+            6 -> binding.six.setImageResource(img)
+            7 -> binding.seven.setImageResource(img)
+            8 -> binding.eight.setImageResource(img)
+            9 -> binding.nine.setImageResource(img)
+        }
+
+        if (game.isWin(game.getCurrentPlayer())) {
+            println("Winner: ${game.getCurrentPlayer()}")
+
+            val builder = AlertDialog.Builder(this)
+                .setTitle("Winner : ${game.getCurrentPlayer()}")
+                .setMessage("Score 1: ${game.getPlayer1Score()} and Score 2 : ${game.getPlayer2Score()}")
+                .setPositiveButton("Play More") { _, _ -> reset() }
+                .setNegativeButton("No") { _, _ ->
+                    Snackbar.make(
+                        binding.root,
+                        "Winner : ${game.getCurrentPlayer()}",
+                        BaseTransientBottomBar.LENGTH_SHORT
+                    ).show()
+                }
+            val alertDialog: AlertDialog = builder.create()
+            alertDialog.setCancelable(false)
+            alertDialog.show()
+
+            if (game.getTurn() == 1) {
+                binding.player1Score.text =
+                    resources.getString(R.string.win_text, game.getPlayer1Score())
+            } else {
+                binding.player2Score.text =
+                    resources.getString(R.string.win_text, game.getPlayer2Score())
+            }
+        }else if (game.isDraw()){
+            game.increaseDrawScore(1)
+            binding.drawScore.text = resources.getString(R.string.draw_text, game.getDrawScore())
+            Snackbar.make(binding.root, "Game draw", BaseTransientBottomBar.LENGTH_SHORT).show()
+            reset()
+        } else {
+            game.nextTurn()
+            binding.currentPlayer.text =
+                resources.getString(R.string.player_turn, game.getCurrentPlayer())
+        }
     }
 }
